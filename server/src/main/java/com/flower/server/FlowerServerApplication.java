98b99d8f5eaf121648cec52737ca9a5228de064a
Ritik Drona <ritik1218@gmail.com>
2025-08-06 22:50:32 +0530

    Backend API Setup

---
+++ server/src/main/java/com/flower/server/FlowerServerApplication.java
@@ -0,0 +1,13 @@
+package com.flower.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/flower/server/FlowerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flower.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
